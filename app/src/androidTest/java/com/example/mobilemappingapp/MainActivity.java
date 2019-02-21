f54702c70d72054f342ae3237e9d8d4623b4f8df
Arsalan Babar <ababar3@uic.edu>
2019-02-21 12:38:23 -0600

    Add files via upload

--- app/src/androidTest/java/com/example/mobilemappingapp/MainActivity.java
+++ app/src/androidTest/java/com/example/mobilemappingapp/MainActivity.java
@@ -16,7 +16,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//Arsalan Babar Test
+//OB
+//Renz
 public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
